91b68c9b64a758238b7f04f3d83062491e8fe6dd
suhas <snani4732@gmail.com>
2024-09-05 23:14:14 +0100

    updated server.js

--- school_proj/server.js
+++ school_proj/server.js
@@ -3,7 +3,7 @@ const app = express()
 let studentArray = require("./initialData")
 
 //middleware
-app.use(express.json)
+app.use(express.json())
 
 app.get("/api/student", (req, res)=>{
     res.send(studentArray)
